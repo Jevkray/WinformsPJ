b6e16620ba4d021775099d93740d001b0a993453
Евгений Красовский <semmitwinsboss@gmail.com>
2023-05-01 12:47:40 +0300

    Done Form11 - Validation + Rewrite full code

--- Praktika/Form11.Designer.cs
+++ Praktika/Form11.Designer.cs
@@ -29,31 +29,31 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel3.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -69,6 +69,16 @@
             this.panel2.Size = new System.Drawing.Size(979, 527);
             this.panel2.TabIndex = 7;
             // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dataGridView1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(7, 7, 7, 1);
+            this.panel4.Size = new System.Drawing.Size(710, 496);
+            this.panel4.TabIndex = 13;
+            // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
@@ -83,28 +93,8 @@
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(696, 488);
             this.dataGridView1.TabIndex = 7;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.dataGridView1);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(0, 0);
-            this.panel4.Name = "panel4";
-            this.panel4.Padding = new System.Windows.Forms.Padding(7, 7, 7, 1);
-            this.panel4.Size = new System.Drawing.Size(710, 496);
-            this.panel4.TabIndex = 13;
-            // 
-            // button1
-            // 
-            this.button1.AutoSize = true;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(6, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(698, 27);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Обновить";
-            this.button1.UseVisualStyleBackColor = true;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
             // panel3
             // 
@@ -117,6 +107,19 @@
             this.panel3.Size = new System.Drawing.Size(710, 31);
             this.panel3.TabIndex = 12;
             // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(6, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(698, 27);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.button2);
@@ -124,14 +127,14 @@
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.textBox7);
-            this.groupBox1.Controls.Add(this.textBox6);
             this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox6);
             this.groupBox1.Controls.Add(this.textBox4);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
@@ -146,6 +149,18 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Данные";
             // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(4, 439);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(261, 28);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // button4
             // 
             this.button4.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -156,6 +171,7 @@
             this.button4.TabIndex = 16;
             this.button4.Text = "Редактировать";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -167,45 +183,45 @@
             this.button3.TabIndex = 15;
             this.button3.Text = "Удалить";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(9, 168);
+            this.label7.Location = new System.Drawing.Point(9, 209);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(61, 16);
+            this.label7.Size = new System.Drawing.Size(50, 16);
             this.label7.TabIndex = 13;
-            this.label7.Text = "Остаток";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.label7.Text = "Сумма";
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(129, 165);
+            this.textBox7.Location = new System.Drawing.Point(129, 206);
             this.textBox7.Margin = new System.Windows.Forms.Padding(4);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(132, 22);
             this.textBox7.TabIndex = 12;
             // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(129, 225);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(132, 22);
-            this.textBox6.TabIndex = 11;
-            // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(129, 193);
+            this.textBox5.Location = new System.Drawing.Point(129, 146);
             this.textBox5.Margin = new System.Windows.Forms.Padding(4);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(132, 22);
-            this.textBox5.TabIndex = 10;
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(129, 176);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(132, 22);
+            this.textBox6.TabIndex = 10;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(129, 132);
+            this.textBox4.Location = new System.Drawing.Point(129, 116);
             this.textBox4.Margin = new System.Windows.Forms.Padding(4);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(132, 22);
@@ -213,7 +229,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(129, 98);
+            this.textBox3.Location = new System.Drawing.Point(129, 86);
             this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(132, 22);
@@ -221,7 +237,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(129, 59);
+            this.textBox2.Location = new System.Drawing.Point(129, 56);
             this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(132, 22);
@@ -234,31 +250,32 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 6;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 228);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(50, 16);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Сумма";
+            this.textBox1.Tag = "";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 196);
+            this.label5.Location = new System.Drawing.Point(9, 149);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 16);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Количество";
+            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Остаток";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 179);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Количество";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 135);
+            this.label4.Location = new System.Drawing.Point(9, 119);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 16);
@@ -268,7 +285,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 101);
+            this.label3.Location = new System.Drawing.Point(9, 89);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 16);
@@ -278,13 +295,12 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 62);
+            this.label2.Location = new System.Drawing.Point(9, 59);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Наименование";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
@@ -296,30 +312,19 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Код";
             // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button2.Location = new System.Drawing.Point(4, 439);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(261, 28);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Добавить";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // Form11
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(979, 527);
             this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form11";
             this.Text = "Запасы";
             this.Load += new System.EventHandler(this.Form11_Load);
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.groupBox1.ResumeLayout(false);
@@ -339,14 +344,14 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
--- Praktika/Form11.cs
+++ Praktika/Form11.cs
@@ -19,6 +19,7 @@ namespace Praktika
         public MySqlCommand _mycom;
         public string _access;
         private string _connectData = "Server=localhost;Database=mydb;Uid=root;pwd=12345;charset=utf8";
+        public string value2;
 
         public DataSet ds;
         private void Buttons(string _access)
@@ -60,93 +61,115 @@ namespace Praktika
             }
         }
         public Form11()
-            { 
-                InitializeComponent();
-                Initialization();
-            }
+        {
+            InitializeComponent();
+            Initialization();
+        }
         public void Initialization()
-            {
-                _mycon = GetDBConnection();
-                _table = new DataTable();
-                Ychet();
-            }
+        {
+            _mycon = GetDBConnection();
+            _table = new DataTable();
+            Ychet();
+        }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-            {
-                dataGridView1.Show();
-            }
+        {
+            dataGridView1.Show();
+        }
         public static MySqlConnection GetDBConnection()
-            {
-
-                string host = "localhost";
-                int port = 3306;
-                string database = "mydb";
-                string username = "root";
-                string password = "12345";
-                try
-                {
-                    return GetDBConnection(host, port, database, username, password);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error: " + e.Message);
-                }
-                return null;
-            }
-        public static MySqlConnection GetDBConnection(string host, int port, string database, string username, string password)
-            {
-                String connString = "Server=" + host + ";Database=" + database
-                    + ";port=" + port + ";User Id=" + username + ";password=" + password;
-
-                MySqlConnection SqlConnection = new MySqlConnection(connString);
-
-                return SqlConnection;
-            }
-         public void MSDataFill(string script, string connect, DataGridView dataGridView)
-            {
-                try
-                {
-                    _table = new DataTable();
-                    MySqlDataAdapter ms_data = new MySqlDataAdapter(script, _mycon);
-                    ms_data.Fill(_table);
-                    dataGridView.DataSource = _table;
-                    _mycon.Close();
-                    //_table.Clear();
-                }
-                catch (Exception exeption)
-                {
-                    MessageBox.Show("" + exeption);
-                }
-            }
-
-            private void Form11_Load(object sender, EventArgs e)
         {
 
+            string host = "localhost";
+            int port = 3306;
+            string database = "mydb";
+            string username = "root";
+            string password = "12345";
+            try
+            {
+                return GetDBConnection(host, port, database, username, password);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            return null;
+        }
+        public static MySqlConnection GetDBConnection(string host, int port, string database, string username, string password)
+        {
+            String connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+            MySqlConnection SqlConnection = new MySqlConnection(connString);
+
+            return SqlConnection;
+        }
+        public void MSDataFill(string script, string connect, DataGridView dataGridView)
+        {
+            try
+            {
+                _table = new DataTable();
+                MySqlDataAdapter ms_data = new MySqlDataAdapter(script, _mycon);
+                ms_data.Fill(_table);
+                dataGridView.DataSource = _table;
+                _mycon.Close();
+                //_table.Clear();
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show("" + exeption);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Ychet();
         }
+        private void Form11_Load(object sender, EventArgs e)
+        {
+            _access = Access.Accesses;
+            Buttons(Access.Accesses);
+        }
         private void Ychet()
         {
-            string script = "Select id,Code as Код,Name as Наименование,EdIzmer as Единица_Измерения,Cost as Цена, RemainEndPer as Остаток, Amount as Количество, Sum as Сумма from RemainingStock";
+            string script = "Select remainingstock.id, remainingstock.Code as Код, remainingstock.Name as Наименование, remainingstock.EdIzmer as Единица_Измерения, remainingstock.Cost as Цена, remainingstock.RemainEndPer as Остаток, remainingstock.Amount as Количество, remainingstock.Sum as Сумма from remainingstock";
             MSDataFill(script, _connectData, dataGridView1);
             dataGridView1.Columns[0].Visible = false;
         }
-
-        private void button1_Click_1(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-
+            string script = $"insert into remainingstock(Code,Name,EdIzmer,Cost,RemainEndPer,Amount,Sum) value ('{textBox1.Text}','{textBox2.Text}','{textBox3.Text}','{textBox4.Text}','{textBox5.Text}','{textBox6.Text}','{textBox7.Text}')";
+            MSDataFill(script, _connectData, dataGridView1);
+            Initialization();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
+            {
+                try
+                {
+                    DialogResult zz = MessageBox.Show("Вы уверены что хотите удалить этот элемент?", "Удаление", MessageBoxButtons.YesNo);
+                    if (zz == DialogResult.Yes)
+                    {
 
+
+                        string script = ($"DELETE FROM remainingstock WHERE ID = {value2} ");
+                        MSDataFill(script, _connectData, dataGridView1);
+                        Initialization();
+                    }
+                }
+                catch { MessageBox.Show("Неверно введены данные"); }
+            }
         }
 
-        private void label7_Click(object sender, EventArgs e)
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
+            value2 = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string script = ($"UPDATE remainingstock SET Code = '{textBox1.Text}', Name = '{textBox2.Text}', EdIzmer = '{textBox3.Text}', Cost = '{textBox4.Text}', RemainEndPer = '{textBox5.Text}', Amount = '{textBox6.Text}', Sum = '{textBox7.Text}' WHERE ID = {value2}");
+            MSDataFill(script, _connectData, dataGridView1);
+            Initialization();
         }
     }
 }
